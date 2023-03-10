578a4f2f6720001573c1e041194485515138b937
ayaal_fedorov <aiaal8964@mail.ru>
2023-03-11 01:06:19 +0900

    some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,13 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="flex w-full justify-around mt-4 items-center p-3">
-        <div class="text-red-500">1</div>
-        <input type="button" value="Please, click me" id="button" class="p-3 px-4 text-white bg-green-400 rounded-sm">
-        <div class="text-green-500">2</div>
+    <div class="flex w-full justify-center mt-4 mb-4">
+        <div class="text-2xl">Hello on my website!</div>
+
+    </div>
+
+    <div class="flex w-full justify-around items-center p-3">
+        <input type="button" value="Please, click me" id="button" class="cursor-pointer hover:bg-green-500 p-3 px-4 text-white bg-green-400 rounded">
     </div>
     <script src="{{asset('js/script.js')}}"></script>
 </body>
