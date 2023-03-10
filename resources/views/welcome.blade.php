f9d781e35a60985a4d7d28a21b5da1d50916adbf
ayaal_fedorov <aiaal8964@mail.ru>
2023-03-11 01:13:40 +0900

    some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="flex w-full justify-center mt-4 mb-4">
+    <div class="flex w-full justify-center mt-4 mb-4 flex-col items-center">
         <div class="text-2xl">Hello on my website!</div>
-
+        <div class="text-lg">You're IP address is: <span class="text-red-500">{{request()->ip()}}</span></div>
     </div>
 
     <div class="flex w-full justify-around items-center p-3">
-        <input type="button" value="Please, click me" id="button" class="cursor-pointer hover:bg-green-500 p-3 px-4 text-white bg-green-400 rounded">
+        <input type="button" value="Please, click me" id="button" class="transition-all cursor-pointer hover:bg-green-500 p-3 px-4 text-white bg-green-400 rounded">
     </div>
     <script src="{{asset('js/script.js')}}"></script>
 </body>
