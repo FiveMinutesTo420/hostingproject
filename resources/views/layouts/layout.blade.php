bd5e2a1515d76b7fc3a80126c0b385072f69fd38
ayaal_fedorov <aiaal8964@mail.ru>
2023-03-12 03:28:48 +0900

    fix bugs search bar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -160,34 +160,31 @@
             </div>
 
             
-            <div class="flex flex-1 w-full">
-                <button class=" bg-[#F8F9FA] border py-3 xl:py-0 px-5 flex rounded-tl rounded-bl items-center">
+            <div class="flex  w-full">
+                <button class=" bg-[#F8F9FA] border py-3  px-5 flex rounded-tl rounded-bl items-center">
                     Везде
                     <span class="flex items-center">
-                        <svg width="21" fill="#aaa" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="21" fill="#aaa" class="" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
                         </svg>
                     </span>
                 </button>
                 <input placeholder="Поиск в каталоге" type="text" class="flex-1 border px-3">
                 <button class="bg-[#F8F9FA] px-4 border rounded-tr rounded-br">				
-                    <svg fill="#aaa" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg fill="#aaa" width="24" class="" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
                 </svg>
-            </button>
+                </button>
             </div>
-            <button class="flex text-white items-center py-1 xl:py-0 bg-[#007BFF] w-full xl:w-auto text-sm px-4 rounded space-x-4">
+            <button class="flex py-3 text-white justify-between  items-center py-1 xl:py-0 bg-[#007BFF] w-full xl:w-auto text-sm px-4 rounded space-x-4">
                 <span class="flex">
-                    <svg width="24" class="" fill="#fff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="24" class=""  fill="#fff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.21 9l-4.38-6.56c-.19-.28-.51-.42-.83-.42-.32 0-.64.14-.83.43L6.79 9H2c-.55 0-1 .45-1 1 0 .09.01.18.04.27l2.54 9.27c.23.84 1 1.46 1.92 1.46h13c.92 0 1.69-.62 1.93-1.46l2.54-9.27L23 10c0-.55-.45-1-1-1h-4.79zM9 9l3-4.4L15 9H9zm3 8c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/>
                     </svg>
                 </span>
-                <div class="flex-1">
-                    <p><strong>0</strong> товар(ов)</p>
-                    <p>на <strong>0.0</strong> руб.</p>
-                </div>
+                <span class="">0 товаров</span>
                 <span class="flex">
-                    <svg width="24" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="24" fill="#fff" class="" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 						<path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
 					</svg>
                 </span>
